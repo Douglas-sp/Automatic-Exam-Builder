343854d286ca0f80708f6ea285b674f2bbe95579
kallyas <kallyasmedia@gmail.com>
2023-06-19 08:49:56 +0300

    fix: major bugs

--- core/models/answer.py
+++ core/models/answer.py
@@ -5,7 +5,7 @@ from core.models.timestamp import Timestamp
 
 class Answer(Timestamp):
     answer_text = models.CharField(max_length=255, blank=False, null=False)
-    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    question = models.ForeignKey('QuestionBank', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.answer_text
