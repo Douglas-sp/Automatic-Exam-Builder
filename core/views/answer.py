2ce0afde32efabbdb3e38e5f49b4a5f8d4ea8d88
Kally <kallyasmedia@gmail.com>
2023-06-19 08:20:16 +0000

    ch: add answers

--- core/views/answer.py
+++ core/views/answer.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from core.models.answer import Answer
+from core.models.question_bank import QuestionBank
+from core.models.exam import Exam
+
 
 
 class AnswerView(TemplateView):
@@ -8,9 +11,22 @@ class AnswerView(TemplateView):
     login_url = reverse_lazy('login')
 
     def get_context_data(self, **kwargs):
-        exam_id = self.kwargs['exam_id']
+        exam_id = self.request.GET.get('exam_id')
+        exam = Exam.objects.get(pk=exam_id)
+        questions = QuestionBank.objects.filter(exam__id=exam_id)
+        answers = Answer.objects.filter(question__exam__id=exam_id)
+        question_answer_list = []
+
+        for question in questions:
+            answer = answers.filter(question=question).first()
+            question_answer_list.append({
+                'question': question.question_text,
+                'answer': answer.answer_text if answer else 'No answer found'
+            })
+
         context = super().get_context_data(**kwargs)
-        context['answers'] = Answer.objects.filter(question__exam__id=exam_id)
+        context['question_answer_list'] = question_answer_list
+        context['exam_name'] = exam.exam_name
         return context
     
     def get(self, request, *args, **kwargs):
