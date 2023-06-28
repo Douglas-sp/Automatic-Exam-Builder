1e12f656da2431b69675f583402ec5d8d6234d68
Douglas-sp <wamaniray@gmail.com>
2023-06-28 10:36:35 +0000

    trial one

--- autoexambuilder/settings.py
+++ autoexambuilder/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-i-g5ua%ekb+wc82rl=+l(x#v!)&%kq+x_w!v@&^h10!kr_vza^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-douglassp-automaticexam-rgk6wufkw4i.ws-eu100.gitpod.io','localhost']
+ALLOWED_HOSTS = ['8000-douglassp-automaticexam-z3w18cohyez.ws-eu101.gitpod.io','8000-douglassp-automaticexam-rgk6wufkw4i.ws-eu100.gitpod.io','localhost']
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-douglassp-automaticexam-rgk6wufkw4i.ws-eu96b.gitpod.io',
     'https://8000-douglassp-automaticexam-rgk6wufkw4i.ws-eu97.gitpod.io',
     'https://8000-douglassp-automaticexam-rgk6wufkw4i.ws-eu99.gitpod.io',
-    'https://8000-douglassp-automaticexam-rgk6wufkw4i.ws-eu100.gitpod.io'
+    'https://8000-douglassp-automaticexam-rgk6wufkw4i.ws-eu100.gitpod.io',
+    'https://8000-douglassp-automaticexam-z3w18cohyez.ws-eu101.gitpod.io'
 ]
 
 
